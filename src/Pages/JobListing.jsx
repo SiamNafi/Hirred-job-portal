d02572ae7bdf6681a335ca459b2e9e8efc2f89cc
Nafiuls <snafiul700@gmail.com>
2025-07-10 20:45:52 +0600

    saved jobs function done now working on serching and filtering companies

--- src/Pages/JobListing.jsx
+++ src/Pages/JobListing.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import { getJobs } from "../api/jobsApi";
+import { BarLoader } from "react-spinners";
+import JobCard from "../components/JobCard";
 
 const JobListing = () => {
-  return <div>JobListing</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+  const { session, isLoaded } = useSession();
+
+  const {
+    fn: fnJobs,
+    data: jobs,
+    loading: loadingJobs,
+    error: errorJobs,
+  } = useFetch(getJobs, { location, company_id, searchQuery });
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnJobs();
+    }
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  if (!isLoaded)
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+
+  return (
+    <div>
+      <h1 className="bg-gradient-to-br from-gray-500 via-gray-200 to-white text-transparent bg-clip-text text-6xl sm:text-7xl text-center  font-extrabold pb-8">
+        Latest Jobs
+      </h1>
+      {/* add filter here */}
+      {loadingJobs && (
+        <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
+      )}
+      {loadingJobs === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => {
+              return (
+                <JobCard
+                  savedInit={job?.saved?.length > 0}
+                  key={job.id}
+                  job={job}
+                ></JobCard>
+              );
+            })
+          ) : (
+            <div>No Jobs Found 😢</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default JobListing;
---
+++ src/components/JobCard.jsx
@@ -0,0 +1,93 @@
+import { useUser } from "@clerk/clerk-react";
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "./ui/card";
+import { Button } from "./ui/button";
+import { Heart, Loader, MapPinIcon, Trash2Icon } from "lucide-react";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { saveJob } from "../api/jobsApi";
+
+const JobCard = ({
+  job,
+  isMyJob = false,
+  savedInit = false,
+  onJobSaved = () => {},
+}) => {
+  const [saved, setSaved] = useState(savedInit);
+  const { user } = useUser();
+  const {
+    fn: fnSavedJob,
+    data: savedJob,
+    loading: loadingSavedJob,
+  } = useFetch(saveJob, { alreadySaved: saved });
+
+  const handleSavedJob = async () => {
+    await fnSavedJob({
+      user_id: user.id, // <-- make sure this is included
+      job_id: job.id,
+    });
+    onJobSaved();
+  };
+
+  useEffect(() => {
+    if (savedJob !== undefined) setSaved(savedJob?.length > 0);
+  }, [savedJob]);
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className={"flex justify-baseline text-2xl font-bold"}>
+          {job.title}
+          {isMyJob && (
+            <Trash2Icon
+              fill="red"
+              size={18}
+              className="text-red-300 cursor-pointer"
+            />
+          )}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className={"flex flex-col gap-4 flex-1"}>
+        <div className="flex justify-between">
+          {job.company && <img src={job.company.logo_url} className="h-6" />}
+          <div className="flex gap-2 items-center">
+            <MapPinIcon size={15} />
+            {job.location}
+          </div>
+        </div>
+        <hr />
+        {job.description.substring(0, job.description.indexOf("."))}
+      </CardContent>
+      <CardFooter className={"flex gap-2"}>
+        <Link to={`/job/${job.id}`} className="flex-1">
+          <Button variant={"secondary"} className={"w-full cursor-pointer"}>
+            More Details
+          </Button>
+        </Link>
+        {!isMyJob && (
+          <Button
+            variant={"outline"}
+            className={"w-15 cursor-pointer"}
+            onClick={handleSavedJob}
+            disabled={loadingSavedJob}
+          >
+            {saved ? (
+              <Heart size={20} stroke="red" fill="red" cursor={"pointer"} />
+            ) : (
+              <Heart size={20} cursor={"pointer"} />
+            )}
+            {loadingSavedJob && <Loader />}
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default JobCard;
